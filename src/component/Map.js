ebffd5e58065581224ce1e27a0ecbf420d2e948d
charade6 <charade6@naver.com>
2022-05-10 03:17:05 +0900

    add mapMarker

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,13 +1,44 @@
+import axios from "axios"
+import { useState, useEffect, useCallback } from "react"
 import MapContainer from "./MapContainer"
 
 function Map() {
+  const [api, setApi] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getApi = useCallback(async () => {
+    const res = await axios({
+      method: "get",
+      url: `/B552584/EvCharger/getChargerInfo?serviceKey=gIgWSDzCeTDpTHNna3UfLVrfBmHbLPDu8IRh%2FvJuoHy5Sp1OFCc9r6uWHIqcEpCF8pWmul9zZMDQLafiKcrx3Q%3D%3D&pageNo=1&numOfRows=100`,
+    })
+    setIsLoading(false)
+    setApi(res.data.items[0].item)
+  }, [])
+
+  useEffect(() => {
+    if (isLoading !== false) {
+      getApi()
+    } else {
+      console.log("로딩끝")
+    }
+  }, [isLoading, getApi])
+
   return (
     <div className="sub-page">
       <div className="banner ban3" />
       <div className="main-content">
         <div className="cont-inner">
           <h2>Map</h2>
-          <MapContainer />
+          {isLoading ? (
+            <div>
+              <h2>로딩중</h2>
+            </div>
+          ) : (
+            <MapContainer
+              lat={api.map((data) => data.lat)}
+              lng={api.map((data) => data.lng)}
+            />
+          )}
         </div>
       </div>
     </div>
--- src/component/MapContainer.js
+++ src/component/MapContainer.js
@@ -2,7 +2,13 @@ import { useEffect } from "react"
 
 const { kakao } = window
 
-const MapContainer = () => {
+const MapContainer = ({ lat, lng }) => {
+  let markerArr = []
+
+  for (let i = 0; i < lat.length; i++) {
+    markerArr[i] = [{ lating: new kakao.maps.LatLng(lat[i], lng[i]) }]
+  }
+
   useEffect(() => {
     // 카카오맵 컨테이너
     const container = document.getElementById("Map")
@@ -12,23 +18,54 @@ const MapContainer = () => {
     }
     const map = new kakao.maps.Map(container, options)
 
+    // 충전소 위치 마킹
+    for (let i = 0; i < markerArr.length; i++) {
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: markerArr[i][0].lating,
+      })
+      marker.setMap(map)
+    }
+
     // 내위치 불러오기
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success)
+      navigator.geolocation.getCurrentPosition(function (pos) {
+        let latitude = pos.coords.latitude
+        let longitude = pos.coords.longitude
+        let accuracy = pos.coords.accuracy
+        let level = map.getLevel()
+        let locPosition = new kakao.maps.LatLng(latitude, longitude)
+        // 위치정확도가 낮을경우 맵레벨 증가
+        if (accuracy > 80) {
+          map.setLevel(
+            (level += Math.round(Math.log(accuracy / 50) / Math.LN2))
+          )
+          console.log(Math.round(Math.log(accuracy / 50) / Math.LN2))
+        }
+        displayMarker(locPosition)
+      })
     }
-    function success(pos) {
-      let latitude = pos.coords.latitude
-      let longitude = pos.coords.longitude
-      let moveLatLon = new kakao.maps.LatLng(latitude, longitude)
-      map.setCenter(moveLatLon)
+    function displayMarker(locPosition) {
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: locPosition,
+      })
+      marker.setMap(map)
+      map.setCenter(locPosition)
     }
     console.log(map)
+
     // 줌 컨트롤
     let zoomControl = new kakao.maps.ZoomControl()
     map.addControl(zoomControl, kakao.maps.ControlPosition.BOTTOMRIGHT)
 
     // 로드뷰
     // map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW)
+
+    // 마커생성
+    // let marker = new kakao.maps.Marker({
+    //   positions: new kakao.maps.LatLng(latArray[i], longArray[i]),
+    // })
   }, [])
 
   return <div id="Map"></div>
