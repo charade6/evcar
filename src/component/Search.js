6f1ba0c0e3de45aef9e5a6a92ee26db1658ef384
charade6 <charade6@naver.com>
2022-06-04 12:39:13 +0900

    add CombinationFilter

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import evcarData from "../asset/evcarData.json"
+// import SearchValue from "./SearchValue"
 
 function Search() {
   const tabList = {
@@ -25,189 +26,179 @@ function Search() {
     efficiency: [3, 4, 5, 6, 7],
     subsidy: ["~ 300", "301 ~ 400", "401 ~ 500", "501 ~ 600", "601 ~ 700"],
   }
-  const tabLength = Object.keys(tabList)
-  const [fltArr, setFltArr] = useState([])
-  // const [fltVal, setFltVal] = useState([])
 
-  // console.log(fltVal)
-  if (!fltArr.length) {
-    console.log("데이터없음")
-  } else {
-    console.log(fltArr)
-  }
-  // console.log(evcarData.carList.filter((e) => e.efficiency === fltArr[0].el))
-  console.log(
-    fltArr.map((e) => {
-      return evcarData.carList.filter((el) => el.maker === e.value)
-    })
-  )
-  const b = []
-  console.log(
-    b.concat(
-      ...fltArr.map((e) => {
-        return evcarData.carList.filter((el) => {
-          switch (e.category) {
-            case "maker":
-              return el.maker === e.value
-            case "vehicleScale":
-              return el.vehicleScale === e.value
-            case "vehicleType":
-              return el.vehicleType === e.value
-            case "efficiency":
-              return e.value <= el.efficiency && el.efficiency < e.value + 1
-            case "subsidy":
+  const [fltArr, setFltArr] = useState([])
+
+  const detailSearch = Object.keys(tabList).map((category, index) => {
+    let tab
+    let ctgName
+    switch (index) {
+      case 0:
+        tab = tabList.maker
+        ctgName = "제조사"
+        break
+      case 1:
+        tab = tabList.vehicleScale
+        ctgName = "차급"
+        break
+      case 2:
+        tab = tabList.vehicleType
+        ctgName = "외형"
+        break
+      case 3:
+        tab = tabList.efficiency
+        ctgName = "전비"
+        break
+      case 4:
+        tab = tabList.subsidy
+        ctgName = "보조금"
+        break
+      default:
+        break
+    }
+
+    return (
+      <dl className="row" key={index}>
+        <dt>{ctgName}</dt>
+        <dd>
+          <ul className="itembox">
+            {tab.map((value, i) => {
               return (
-                parseInt(e.value.slice(-3)) - 100 < el.subsidy &&
-                el.subsidy <= parseInt(e.value.slice(-3))
+                <li key={i}>
+                  <input
+                    type="checkbox"
+                    onChange={(e) => {
+                      if (e.currentTarget.checked) {
+                        setFltArr(fltArr.concat({ category, value }))
+                      } else {
+                        setFltArr(fltArr.filter((item) => item.value !== value))
+                      }
+                    }}
+                  />
+                  <span>{value}</span>
+                </li>
               )
-            default:
-              break
-          }
-        })
-      })
+            })}
+          </ul>
+        </dd>
+      </dl>
     )
-  )
+  })
+
+  const categoryCnt = {
+    maker: [],
+    vehicleScale: [],
+    vehicleType: [],
+    efficiency: [],
+    subsidy: [],
+  }
+
+  fltArr.forEach((e) => {
+    switch (e.category) {
+      case "maker":
+        categoryCnt.maker.push(
+          ...evcarData.carList.filter((el) => el.maker === e.value)
+        )
+        break
+      case "vehicleScale":
+        categoryCnt.vehicleScale.push(
+          ...evcarData.carList.filter((el) => el.vehicleScale === e.value)
+        )
+        break
+      case "vehicleType":
+        categoryCnt.vehicleType.push(
+          ...evcarData.carList.filter((el) => el.vehicleType === e.value)
+        )
+        break
+      case "efficiency":
+        categoryCnt.efficiency.push(
+          ...evcarData.carList.filter((el) =>
+            el.efficiency.toString().indexOf(e.value, 0) ? false : true
+          )
+        )
+        break
+      case "subsidy":
+        categoryCnt.subsidy.push(
+          ...evcarData.carList.filter(
+            (el) =>
+              el.subsidy <= e.value.slice(-3) &&
+              el.subsidy > e.value.slice(-3) - 100
+          )
+        )
+        break
+
+      default:
+        break
+    }
+  })
+  let updateList = [...evcarData.carList]
+
+  if (categoryCnt.maker.length) {
+    updateList = updateList.filter((e) => categoryCnt.maker.includes(e))
+  }
+  if (categoryCnt.vehicleScale.length) {
+    updateList = updateList.filter((e) => categoryCnt.vehicleScale.includes(e))
+  }
+  if (categoryCnt.vehicleType.length) {
+    updateList = updateList.filter((e) => categoryCnt.vehicleType.includes(e))
+  }
+  if (categoryCnt.efficiency.length) {
+    updateList = updateList.filter((e) => categoryCnt.efficiency.includes(e))
+  }
+  if (categoryCnt.subsidy.length) {
+    updateList = updateList.filter((e) => categoryCnt.subsidy.includes(e))
+  }
 
   return (
     <div className="sub-page">
       <div className="banner ban2" />
       <div className="main-content">
         <div className="cont-inner">
-          <h2>Search</h2>
+          <h2>전기차 검색</h2>
           <div className="search_cont">
-            <div className="cont_detailsearch">
-              {tabLength.map((category, index) => {
-                let tab
-                let catName
-                switch (index) {
-                  case 0:
-                    tab = tabList.maker
-                    catName = "제조사"
-                    break
-                  case 1:
-                    tab = tabList.vehicleScale
-                    catName = "차급"
-                    break
-                  case 2:
-                    tab = tabList.vehicleType
-                    catName = "외형"
-                    break
-                  case 3:
-                    tab = tabList.efficiency
-                    catName = "전비"
-                    break
-                  case 4:
-                    tab = tabList.subsidy
-                    catName = "보조금"
-                    break
-                  default:
-                    break
-                }
-
-                return (
-                  <dl className="row" key={index}>
-                    <dt>{catName}</dt>
-                    <dd>
-                      <ul className="itembox">
-                        {tab.map((value, i) => {
-                          return (
-                            <li key={i}>
-                              <input
-                                type="checkbox"
-                                onChange={(e) => {
-                                  if (e.currentTarget.checked) {
-                                    setFltArr(
-                                      fltArr.concat({ category, value })
-                                    )
-                                  } else {
-                                    setFltArr(
-                                      fltArr.filter(
-                                        (item) => item.value !== value
-                                      )
-                                    )
-                                  }
-                                }}
-                              />
-                              <span>{value}</span>
-                            </li>
-                          )
-                        })}
-                      </ul>
-                    </dd>
-                  </dl>
-                )
-              })}
-            </div>
+            <div className="cont_detailsearch">{detailSearch}</div>
 
             <div className="cont_carList">
               <ul>
-                {!fltArr.length ? (
-                  <div>업음</div>
-                ) : (
-                  evcarData.carList
-                    .filter((e) => {
-                      switch (fltArr[0].category) {
-                        case "maker":
-                          return e.maker === fltArr[0].value
-                        case "vehicleScale":
-                          return e.vehicleScale === fltArr[0].value
-                        case "vehicleType":
-                          return e.vehicleType === fltArr[0].value
-                        case "efficiency":
-                          return (
-                            fltArr[0].value <= e.efficiency &&
-                            e.efficiency < fltArr[0].value + 1
-                          )
-                        case "subsidy":
-                          return (
-                            parseInt(fltArr[0].value.slice(-3)) - 100 <
-                              e.subsidy &&
-                            e.subsidy <= parseInt(fltArr[0].value.slice(-3))
-                          )
-                        default:
-                          break
-                      }
-                    })
-                    .map((list, index) => {
-                      return (
-                        <li className="items" key={index}>
-                          <div className="item-box">
-                            <img src={list.img} alt={list.name} />
+                {updateList.map((e, index) => {
+                  return (
+                    <li className="items" key={index}>
+                      <div className="item-box">
+                        <img src={e.img} alt={e.name} />
 
-                            <div className="item-detail">
-                              <p>
-                                {list.maker}
-                                &nbsp;&nbsp;
-                                {list.name}
-                              </p>
-                              <div className="spec">
-                                <span>
-                                  {list.vehicleScale}/{list.vehicleType}
-                                </span>
-                                <span>{list.people}인승</span>
-                                <span>최고속도 {list.distance}km/h</span>
-                                <span>
-                                  (상온) {list.mile_roomtmp}km (저온)
-                                  {list.mile_coldtmp}km
-                                </span>
-                                <span>복합전비 {list.efficiency}</span>
-                                <span>
-                                  {list.battery_type}({list.capacity}kWh)
-                                </span>
-                                <br />
-                                <span>제조사번호 {list.tel}</span>
-                              </div>
-                            </div>
-                            <p>
-                              보조금
-                              <br />
-                              {list.subsidy} 만원
-                            </p>
+                        <div className="item-detail">
+                          <p>
+                            {e.maker}
+                            &nbsp;&nbsp;
+                            {e.name}
+                          </p>
+                          <div className="spec">
+                            <span>
+                              {e.vehicleScale}/{e.vehicleType}
+                            </span>
+                            <span>{e.people}인승</span>
+                            <span>최고속도 {e.distance}km/h</span>
+                            <span>
+                              (상온) {e.mile_roomtmp}km (저온)
+                              {e.mile_coldtmp}km
+                            </span>
+                            <span>복합전비 {e.efficiency}</span>
+                            <span>
+                              {e.battery_type}({e.capacity}kWh)
+                            </span>
+                            <br />
+                            <span>제조사번호 {e.tel}</span>
                           </div>
-                        </li>
-                      )
-                    })
-                )}
+                        </div>
+                        <p>
+                          보조금
+                          <br />
+                          {e.subsidy} 만원
+                        </p>
+                      </div>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
           </div>
