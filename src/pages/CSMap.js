187de795ce33a566ac4579b7588d6ac4c1bf1625
charade6 <charade6@naver.com>
2022-06-05 02:41:40 +0900

    netlify setting

--- src/pages/CSMap.js
+++ src/pages/CSMap.js
@@ -7,10 +7,13 @@ function CSMap() {
   const [api, setApi] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
 
+  const PROXY = window.location.hostname === "localhost" ? "" : "/proxy"
+  const URL = `${PROXY}/B552584/EvCharger/getChargerInfo?serviceKey=gIgWSDzCeTDpTHNna3UfLVrfBmHbLPDu8IRh%2FvJuoHy5Sp1OFCc9r6uWHIqcEpCF8pWmul9zZMDQLafiKcrx3Q%3D%3D&pageNo=1&numOfRows=9999`
+
   const getApi = useCallback(async () => {
     const res = await axios({
       method: "get",
-      url: `B552584/EvCharger/getChargerInfo?serviceKey=gIgWSDzCeTDpTHNna3UfLVrfBmHbLPDu8IRh%2FvJuoHy5Sp1OFCc9r6uWHIqcEpCF8pWmul9zZMDQLafiKcrx3Q%3D%3D&pageNo=1&numOfRows=9999`,
+      url: URL,
     })
     setApi(res.data.items[0].item)
     console.log(res.data.items[0].item)
