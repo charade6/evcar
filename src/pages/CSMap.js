7100f61b7d6c5d33c0c26bec7bfd58ba54a50574
charade6 <charade6@naver.com>
2022-06-05 03:00:43 +0900

    api오류

--- src/pages/CSMap.js
+++ src/pages/CSMap.js
@@ -7,18 +7,15 @@ function CSMap() {
   const [api, setApi] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
 
-  const PROXY = window.location.hostname === "localhost" ? "" : "/proxy"
-  const URL = `${PROXY}/B552584/EvCharger/getChargerInfo?serviceKey=gIgWSDzCeTDpTHNna3UfLVrfBmHbLPDu8IRh%2FvJuoHy5Sp1OFCc9r6uWHIqcEpCF8pWmul9zZMDQLafiKcrx3Q%3D%3D&pageNo=1&numOfRows=9999`
-
   const getApi = useCallback(async () => {
     const res = await axios({
       method: "get",
-      url: URL,
+      url: `/B552584/EvCharger/getChargerInfo?serviceKey=gIgWSDzCeTDpTHNna3UfLVrfBmHbLPDu8IRh%2FvJuoHy5Sp1OFCc9r6uWHIqcEpCF8pWmul9zZMDQLafiKcrx3Q%3D%3D&pageNo=1&numOfRows=9999`,
     })
     console.log(res)
     setApi(res.data.items[0].item)
     setIsLoading(false)
-  }, [URL])
+  }, [])
 
   useEffect(() => {
     isLoading ? getApi() : console.log("init")
